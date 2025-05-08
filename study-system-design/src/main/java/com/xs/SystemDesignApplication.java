24a01f218f410b187f9fb4b4f158dca3ca35a332
xiaoshenaixuexi <1308798543@qq.com>
2025-05-08 17:36:56 +0800

    系统设计-基础内存搭建

---
+++ study-system-design/src/main/java/com/xs/SystemDesignApplication.java
@@ -0,0 +1,23 @@
+package com.xs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: SystemDesignApplication
+ * Package: com.xs
+ * Description:简易实现两个系统设计demo：防盗链+短链
+ *
+ * @Author 高伟
+ * @Create 2025/5/7 18:51
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+public class SystemDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemDesignApplication.class, args);
+    }
+
+}
