e05310e962600b2e83b5fd6ce3fc3afbdd2df7f9
xiaoshenaixuexi <1308798543@qq.com>
2025-08-21 16:14:52 +0800

    主启动&pom文件

---
+++ study-ssm/src/main/java/com/xs/StudySsmApplication.java
@@ -0,0 +1,21 @@
+package com.xs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: StudySsmApplication
+ * Package: com.xs
+ * Description:
+ *
+ * @Author 高伟
+ * @Create 2025/8/12 17:23
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+public class StudySsmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudySsmApplication.class, args);
+    }
+}
